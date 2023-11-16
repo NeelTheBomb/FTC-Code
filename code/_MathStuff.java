ae6a165e6737b7b24bc4e6346a5f147629f4135f
caveat <91349837+caveat-dev@users.noreply.github.com>
2023-11-15 20:21:05 -0600

    Update _MathStuff.java

--- code/_MathStuff.java
+++ code/_MathStuff.java
@@ -2,11 +2,9 @@ package org.firstinspires.ftc.teamcode.Event2023;
 
 
 public class MathStuff {
-    public static double remap_range(double old_min, double old_max, double new_min, 
-            double new_max, double value) {
+    public static double remap_range(double old_min, double old_max, double new_min, double new_max, double value) {
         
-        return new_min + (value - old_min) * ((new_max - new_min) 
-                / (old_max - old_min));
+        return new_min + (value - old_min) * ((new_max - new_min) / (old_max - old_min));
     }
     
     
